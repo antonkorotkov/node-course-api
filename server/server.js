f658cae5e1e10dd7115da6acb5ba5839c7f55407
Anton <antonkorotkoff@gmail.com>
2019-01-25 22:05:56 +0200

    get by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
@@ -25,6 +26,28 @@ app.get('/todos', (req, res) => {
   Todo.find({}).then((todos) => {
     res.send({todos})
   }, (e) => {
+    res.status(400).send(e)
+  })
+})
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
+
+  if (!ObjectID.isValid(id)) {
+    return res
+      .status(404)
+      .send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (todo) {
+      return res.status(200).send({todo})
+    }
+
+    res.status(404).send()
+  }, (e) => {
+    res.status(400).send(e)
+  }).catch((e) => {
     res.status(400).send(e)
   })
 })
